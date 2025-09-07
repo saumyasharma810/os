0f666f8c985f96601e49932d3710fd5351e995aa
saumyasharma810 <saumya.sharma810@gmail.com>
2025-09-07 23:55:12 +0530

    Add fifo scheduler in main

--- scheduler/main.cpp
+++ scheduler/main.cpp
@@ -2,14 +2,24 @@
 #include <vector>
 #include "process.h"
 #include "scheduler.h"
+#include "fifo.cpp"
 
 int main() {
     // Step 1: Create some sample processes
     std::vector<Process> processes = {
-        {1, 0, 4, 0},   // pid=1, arrival=0, burst=4
-        {2, 1, 5, 0},   // pid=2, arrival=1, burst=5
-        {3, 2, 2, 0}    // pid=3, arrival=2, burst=2
+    {1, 12, 5, 2},   // PID=1, Arrival=12, Burst=5
+    {2, 0, 4, 1},    // PID=2, Arrival=0, Burst=4  
+    {3, 20, 3, 3},   // PID=3, Arrival=20, Burst=3  
+    {4, 6, 2, 2},    // PID=4, Arrival=6, Burst=2  
+    {5, 8, 6, 1},    // PID=5, Arrival=8, Burst=6  
+    {6, 3, 4, 2},    // PID=6, Arrival=3, Burst=4  
+    {7, 100, 5, 3},   // PID=7, Arrival=14, Burst=5  
+    {8, 1, 2, 1},    // PID=8, Arrival=1, Burst=2  
+    {9, 18, 4, 2},   // PID=9, Arrival=18, Burst=4  
+    {10, 5, 3, 1}    // PID=10, Arrival=5, Burst=3  
     };
+
+
 
     // Step 2: Print the initial process list
     std::cout << "Initial Processes:\n";
@@ -21,7 +31,17 @@ int main() {
                   << p.priority << "\n";
     }
 
-    std::cout << "\n[Scheduler not yet implemented]\n";
+
+    // Step 3: Initialize schedulers
+    FIFO fifo_scheduler(processes); 
+    fifo_scheduler.schedule();
+    fifo_scheduler.print();
+    fifo_scheduler.printAverageResponseTime();
+    fifo_scheduler.printAverageTurnaroundTime();
+    fifo_scheduler.printAverageWaitingTime();
+
+    
+
 
     return 0;
 }
--- scheduler/scheduler.h
+++ scheduler/scheduler.h
@@ -2,18 +2,31 @@
 #ifndef SCHEDULER_H
 #define SCHEDULER_H
 
-#include <vector>
 #include "process.h"
 
 class Scheduler {
 public:
 
-    // Run the scheduling algorithm on the given processes
-    virtual void schedule(std::vector<Process>& processes) = 0;
+    // // Run the scheduling algorithm on the given processes
+    virtual void schedule() = 0;
 
     // Optional: calculate average waiting/turnaround time
-    virtual double averageWaitingTime(const std::vector<Process>& processes);
-    virtual double averageTurnaroundTime(const std::vector<Process>& processes);
+    virtual double averageWaitingTime() = 0;
+    virtual double averageTurnaroundTime() = 0;
+    virtual double averageResponseTime() = 0;
+
+    void printAverageWaitingTime(){
+        std::cout << "Average Waiting Time is "<< averageWaitingTime() <<"\n";
+    }
+
+    void printAverageResponseTime(){
+        std::cout << "Average Response Time is "<< averageResponseTime() <<"\n";
+    }
+
+    void printAverageTurnaroundTime(){
+        std::cout << "Average Turnaround Time is "<< averageTurnaroundTime() <<"\n";
+    }
+
 };
 
 #endif
